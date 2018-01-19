120625aa99604a88c92b6cf029281f3a11d80b9a
kgentner <gentnerkarl@gmail.com>
2018-01-19 14:34:05 -0800

    code style changes

--- test/counter_test.js
+++ test/counter_test.js
@@ -3,14 +3,14 @@
 var expect = require('chai').expect;
 var Counter = require('../lib/Counter');
 
-describe('Test the Counter methods', function () {
+describe('Test the Counter methods', function() {
   var c = new Counter();
 
-  it('gets an accurate initial output', function () {
+  it('gets an accurate initial output', function() {
     expect(c.getValue()).to.eql(0);
   });
 
-  it('gets an accurate output after incrementing', function () {
+  it('gets an accurate output after incrementing', function() {
     c.increment();
     expect(c.getValue()).to.eql(1);
   });
